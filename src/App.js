fb1b5e17afd76e755b23d0c4c4364bd25886cb25
manviag05 <agarwal05.manvi@gmail.com>
2023-08-13 13:57:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Mycomponenets/Header';
+import
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    <Todos/>
+    <Todoitem/>
+    </>
+  );
+}
+
+export default App;
