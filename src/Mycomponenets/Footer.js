d5f5016199500fee01e03a56e45ec0ac68b2dd62
manviag05 <agarwal05.manvi@gmail.com>
2023-08-14 19:03:20 +0530

    recent1

--- src/Mycomponenets/Footer.js
+++ src/Mycomponenets/Footer.js
@@ -1,15 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-  let footerStyle={
-    position:"absolute",
-    top:"100vh",
-    width:"100%"
-  }
+ 
 
 
   return (
-    <footer className="bg-dark text-light py-3" style={footerStyle}>
+    <footer className="bg-dark text-light py-3" >
       <p className="text-center">
       Copyright &copy; MyTodolist.com
       </p>
--- src/Mycomponenets/Todos.js
+++ src/Mycomponenets/Todos.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Todoitem from './Todoitem';
 const Todos = (props) => {
+  let mystyle={
+    minheight: "70vh",
+    margin:"40px auto"
+  }
+
+
   return (
-    <div className="container">
+    <div className="container my-3" style={mystyle}>
         <h3 className=" my-3">Todo list</h3>
       {props.todos.length===0?" No todos to diaplay":props.todos.map((todo)=>{
-        return <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-      })}
+        return(
+          
+          <>
+          <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete}/> <hr/>
+     </> )})
+    }
     </div>
   )
 }
