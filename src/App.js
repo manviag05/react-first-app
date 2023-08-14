7323ad0edf2242531fa3508f23f17cd6ae9fb073
manviag05 <agarwal05.manvi@gmail.com>
2023-08-14 19:02:48 +0530

    recent

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import './App.css';
 import Header from './Mycomponenets/Header';
 import Todos from './Mycomponenets/Todos';
 import Footer from './Mycomponenets/Footer';
+import { useState } from 'react';
+import Addtodo from './Mycomponenets/Addtodo';
 
 function App() {
 const onDelete=(todo)=>{
   console.log("i am delete of todo",todo);
+
+  setTodos(todos.filter((e)=>{
+return e!==todo;
+    }))
 }
 
-let todos=[
+
+const addTodo=(title,desc)=>{
+  console.log("i am adding this todo",title,desc)
+  let sno;
+  if(todos.length==0){
+    sno=0;
+  }
+  else{
+    sno=todos[todos.length-1].sno+1;
+  }
+  const mytodos={   //defining additional todo
+    sno:sno,
+    title:title,
+    desc:desc
+  }
+  setTodos([...todos, mytodos]);   //adding todo to todos list.
+  console.log(mytodos);
+}
+
+const[todos , setTodos]=useState([
   {
     sno:1,
     title:"go to market",
@@ -22,18 +47,18 @@ let todos=[
   },
   {
     sno:3,
-    title:"go to store",
+    title:"go to shop",
     desc:"you have to go store"
   }
-]
-
+])
+  
 
   return (
     <>
     <Header title="my todo list" searchBar={false} />
-    <Footer />
+    <Addtodo addTodo={addTodo}/>
     <Todos todos={todos} onDelete={onDelete}/>
-   
+    <Footer />
     </>
   );
 }
--- src/Mycomponenets/Todos.js
+++ src/Mycomponenets/Todos.js
@@ -3,8 +3,8 @@ import Todoitem from './Todoitem';
 const Todos = (props) => {
   return (
     <div className="container">
-        <h3 className="text-center my-3">Todo list</h3>
-      {props.todos.map((todo)=>{
+        <h3 className=" my-3">Todo list</h3>
+      {props.todos.length===0?" No todos to diaplay":props.todos.map((todo)=>{
         return <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
       })}
     </div>
